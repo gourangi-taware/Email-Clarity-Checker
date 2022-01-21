64051a16086ff15f97ae7a4dab465b51294db809
Gourangi Taware <gourangi0309@gmail.com>
2022-01-21 17:10:34 +0530

    Email Clarity Checker

---
+++ Email Clarity Checker only HTML,CSS/js/postrequest.js
@@ -0,0 +1,163 @@
+$(document).ready(function () {
+  
+  $("#submitEmailClarifier").click(function (event) {
+    
+    if($("#emailbody").val()=="")
+    {
+      window.alert("Enter the Email body in textarea");
+    }
+    else
+    {
+    event.preventDefault();
+    ajaxPostToEmailClarifier();
+    }
+  });
+  $("#empty").click(function (event) {
+    event.preventDefault();
+    $("#small-dialog").css("display", "none");
+    $("#emailbody").val("");
+   
+    $("textarea").removeClass("hwt-content");
+  });
+  $("#gradebutton").click(function (event) {
+    
+    event.preventDefault();
+    $("#words").css("display", "none");
+    $("#issues").css("display", "none");
+    $("#grade").css("display", "block");
+    $("#gradebutton").css("background-color", "#C0C0C0");
+    $("#wordsbutton").css("background-color", "#807d7d");
+    $("#issuesbutton").css("background-color", "#807d7d");
+    
+  });
+  $("#wordsbutton").click(function (event) {
+    
+    event.preventDefault();
+    $("#grade").css("display", "none");
+    $("#issues").css("display", "none");
+    $("#words").css("display", "block");
+    $("#wordsbutton").css("background-color", "#C0C0C0");
+    $("#gradebutton").css("background-color", "#807d7d");
+    $("#issuesbutton").css("background-color", "#807d7d");
+    
+  });
+  $("#issuesbutton").click(function (event) {
+    
+    event.preventDefault();
+    $("#words").css("display", "none");
+    $("#grade").css("display", "none");
+    $("#issues").css("display", "block");
+    $("#issuesbutton").css("background-color", "#C0C0C0");
+    $("#wordsbutton").css("background-color", "#807d7d");
+    $("#gradebutton").css("background-color", "#807d7d");
+    
+  });
+  
+
+  async function ajaxPostToEmailClarifier() {
+    // PREPARE FORM DATA
+    $("#loading-gif").css("display", "inline");
+    var emailbody=$("#emailbody").val()
+
+    var params = {
+      AccountId: "AccountId", // your AWS user account ID
+      RoleArn:
+        "arn:aws:iam::AccountId:role/Cognito_EmailClarityCheckerUnauth_Role", // your Cognito unauth role (it wil look something like this)
+      IdentityPoolId: "IdentityPoolId", // the identity pool id found in Cognito
+    };
+
+    // set the Amazon Cognito region
+    AWS.config.region = "us-east-1";
+    // initialize the Credentials object with our parameters
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials(params);
+
+    // invoking the actual Lambda function
+    var lambda = new AWS.Lambda();
+    invoke_response = lambda.invoke(
+      {
+        FunctionName: "emailClarityChecker", // your function name
+        LogType: "Tail",
+        InvocationType: "RequestResponse",
+        Payload: JSON.stringify({
+          emailbody: emailbody,
+        }), // how you send the email to the Lambda event
+      },
+      function (err, data) {
+        if (err) {
+          window.alert("Server Error.Please try again.");
+          console.log(err, err.stack);
+        } else {
+          try{ 
+           resultant=JSON.parse(JSON.parse(data.Payload).body);
+          
+         ajaxP2(resultant);
+        }catch{
+          window.alert("Server Error.Please try again.");
+          $("#loading-gif").css("display", "none");
+      }
+
+      }
+
+      }
+    );
+
+    async function ajaxP2(answers) {
+      $("#words").html("");
+      $("#grade").html("");
+      $("#issuesul").html("");
+      $("#issuesul1").html("");
+      $("#issuesul2").html("");
+      
+      $("#small-dialog").css("display", "block");
+      $("#loading-gif").css("display", "none");
+      var highlightsArray=[
+        {
+          highlight: answers.long_sentences_morethan40,
+          className: 'greaterthan40'
+        },
+        {
+          highlight: answers.long_sentences_morethan30,
+          className: 'greaterthan30'
+        },
+        {
+          highlight: answers.check,
+          className: 'spelling'
+        }
+      ];
+      
+
+      for(let i=0;i<answers.suggestions.length;i++)
+      {
+        var high={
+          highlight: [answers.suggestions[i].index,answers.suggestions[i].index+answers.suggestions[i].offset],
+          className: 'suggestion'
+        }
+        highlightsArray.push(high);
+        if(i==answers.suggestions.length-1)
+        {
+          
+          $("#emailbody").highlightWithinTextarea({
+            highlight: highlightsArray
+           
+        });
+        
+        }
+      }
+                $("#navbar").css("display", "block");
+               $("#words").append("<ul><li><p>Characters Count: "+ answers.characters+" </p></li><li><p>Letters Count: "+ answers.letters+"</p></li><li><p>Syllables Count: "+ answers.syllables+" </p></li><li><p>Words Count: "+ answers.words+"</p></li><li><p>Numbers Count: "+ answers.numbers+"</p></li><li><p>Lines Count: "+ answers.lines+"</p></li><li><p>Sentence Count: "+ answers.sentenceCount+"</p></li><li><p>Nouns Count: "+ answers.nouns+"</p></li><li><p>Verbs Count: "+ answers.verbs+"</p></li><li><p>Adjectives Count: "+ answers.adjectives+"</p></li><li><p>Adverbs Count: "+ answers.adverbs+"</p></li><li><p>Rest Words Count: "+ answers.rest+"</p></li></ul>");
+                $("#grade").append("<ul><li><p>Flesch Reading Ease: "+ answers.fleschReadingEase+" </p></li><li><p>Flesch Kincaid Grade: "+ answers.fleschKincaidGrade+"</p></li><li><p>Coleman Liau Index: "+ answers.colemanLiauIndex+" </p></li><li><p>Automated Readability Index: "+ answers.automatedReadabilityIndex+"</p></li><li><p>Dale Chall Readability Score: "+ answers.daleChallReadabilityScore+"</p></li><li><p>Difficult Words: "+ answers.difficultWords+"</p></li><li><p>Linsear Write Formula: "+ answers.linsearWriteFormula+"</p></li><li><p>Gunning Fog: "+ answers.gunningFog+"</p></li><li><p>Text Standard: "+ answers.textStandard+"</p></li><li><p>Smog Index: "+ answers.smogIndex+"</p></li></ul>");
+                $("#issuesul1").append("<H2>Possible Spelling Issues</H2>");
+                $("#issuesul1").append("<li ><u>Number of spelling mistakes:   "+answers.check.length +"</u></li>");
+                
+                $("#issuesul").append("<H2>Suggestions</H2>");
+                for(let i=0;i<answers.suggestions.length;i++)
+                {
+                  $("#issuesul").append("<li>"+answers.suggestions[i].reason+"</li>")
+                }
+                var lenMorethan40=answers.long_sentences_morethan40.length-1;
+                $("#issuesul2").append("<H2 style='margin-top:8%;'>Readability Issues</H2>");
+                $("#issuesul2").append("<li style='background-color:#fafa72'>Sentences > 40 Syllables:   "+ lenMorethan40 +"</li>");
+                $("#issuesul2").append("<li style='background-color:rgb(185, 243, 130);'>Sentences > 30 Syllables:    "+answers.long_sentences_morethan30.length +"</li>");
+    }
+  }
+});
---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require("express");
+var path = require("path");
+var handlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+const { engine } = require('express-handlebars');
+const lc = require('letter-count');
+const syllables = require('syllables');
+var WordPOS = require('wordpos'),
+wordpos = new WordPOS();
+const rs = require('text-readability');
+var hbs= require( 'express-handlebars' );
+const spell = require('spell-checker-js')
+var writeGood = require('write-good');
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.engine('handlebars', engine({ extname: '.handlebars', defaultLayout: false,partialsDir: __dirname + '/views/partials/'}));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile("home.html", { root: __dirname + "/views" });
+});
+
+app.post("/submit", async function (req, res) {
+  
+  var ans={};
+  ans.characters=lc.count(req.body.emailbody).chars;
+  ans.letters=lc.count(req.body.emailbody).letters;
+  ans.syllables=syllables(req.body.emailbody);
+  ans.words=lc.count(req.body.emailbody).words;
+  ans.numbers=lc.count(req.body.emailbody).numbers;
+  ans.lines=lc.count(req.body.emailbody).lines;
+  ans.fleschReadingEase=rs.fleschReadingEase(req.body.emailbody)
+  ans.fleschKincaidGrade=rs.fleschKincaidGrade(req.body.emailbody)
+  ans.colemanLiauIndex=rs.colemanLiauIndex(req.body.emailbody)
+  ans.automatedReadabilityIndex=rs.automatedReadabilityIndex(req.body.emailbody)
+  ans.daleChallReadabilityScore=rs.daleChallReadabilityScore(req.body.emailbody)
+  ans.difficultWords=rs.difficultWords(req.body.emailbody)
+  ans.linsearWriteFormula=rs.linsearWriteFormula(req.body.emailbody)
+  ans.gunningFog=rs.gunningFog(req.body.emailbody)
+  ans.textStandard=rs.textStandard(req.body.emailbody)
+  ans.sentenceCount=rs.sentenceCount(req.body.emailbody)
+  ans.fleschReadingEase=rs.fleschReadingEase(req.body.emailbody)
+  ans.smogIndex=rs.smogIndex(req.body.emailbody)
+  ans.automatedReadabilityIndex=rs.automatedReadabilityIndex(req.body.emailbody)
+  ans.daleChallReadabilityScore=rs.daleChallReadabilityScore(req.body.emailbody)
+  
+  var nonewline=req.body.emailbody.replace(/(\r\n|\n|\r)/gm, "");
+   long_sentences=nonewline.split(".");
+ 
+  var long_sentences_morethan30=[];
+  var long_sentences_morethan20=[];
+  for(let i=0;i<long_sentences.length;i++)
+  {
+    var syllable=syllables(long_sentences[i])
+    if(syllable>40)
+    {
+      //console.log(syllable);
+      long_sentences_morethan30.push(long_sentences[i]);
+    }
+    else if(syllable>30 && syllable<40)
+    {
+      long_sentences_morethan20.push(long_sentences[i]);
+    }
+    if(i==long_sentences.length-1)
+    {
+       ans.long_sentences_morethan30=long_sentences_morethan30;
+      ans.long_sentences_morethan20=long_sentences_morethan20;
+    }
+  }
+  
+  
+
+  var suggestions = writeGood(req.body.emailbody);
+  ans.suggestions=suggestions;
+  // Load dictionary
+  spell.load('en')
+  
+  // Checking text
+  const check = spell.check(req.body.emailbody)
+  ans.check=check;
+ wordpos.getPOS(req.body.emailbody).then(function(result) {
+  ans.nouns=result.nouns.length
+  ans.verbs=result.verbs.length
+  ans.adjectives=result.adjectives.length
+  ans.adverbs=result.adverbs.length
+  ans.rest= result.rest.length
+  console.log(ans);
+  return res.send(ans);
+})
+
+  
+  
+  
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("server running at port " + port));
